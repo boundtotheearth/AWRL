99668b97c27df7b29ab93791ce8889e3001790a9
boundtotheearth <farrellnah@gmail.com>
2023-01-06 11:50:23 +0800

    Update training script

--- mind_trap_train.sh
+++ mind_trap_train.sh
@@ -10,9 +10,12 @@ export PATH="/home/f/farnah/miniconda3/bin:$PATH"
 source activate AWRL_env
 #conda activate AWRL_env
 
-python train.py --total-timesteps=200000 \
+python train.py --total-timesteps=600 \
 --map-name="Maps/Mind_Trap.json" \
 --n-steps=512 \
 --batch-size=64 \
 --max-steps=1000 \
 --max-eval-steps=1000 \
+--n-envs=1 \
+--n-eval-envs=1 \
+--n-eval-episodes=10
