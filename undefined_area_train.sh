aee6a9a05b84f112a158689295957c4aff58aa85
boundtotheearth <farrellnah@gmail.com>
2023-01-16 10:27:55 +0800

    Update training script

--- undefined_area_train.sh
+++ undefined_area_train.sh
@@ -10,8 +10,10 @@ export PATH="/home/f/farnah/miniconda3/bin:$PATH"
 source activate AWRL_env
 #conda activate AWRL_env
 
-python train.py --total-timesteps=100000 \
+python train.py \
+--n-iters=100 \
 --map-name="Maps/Undefined_Area.json" \
+--load-opponents='opponents' \
 --lr=0.00003 \
 --n-steps=512 \
 --batch-size=128 \
@@ -20,4 +22,5 @@ python train.py --total-timesteps=100000 \
 --n-eval-episodes=10 \
 --n-envs=10 \
 --n-eval-envs=10 \
---eval-freq=8192
+--eval-freq=8192 \
+--reward-threshold=1
